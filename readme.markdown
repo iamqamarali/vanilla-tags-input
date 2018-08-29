74087aba4d9c46417ef5c2b968443217e791d58a
Qamar Ali <qamar.ali9898@gmail.com>
2018-08-28 21:55:07 -0700

    Add init and destroy function and created readme.md

--- readme.markdown
+++ readme.markdown
@@ -1,6 +1,87 @@
-# JavaScript Tags Input Plugin
-It is a simple plugin for JavaScript to making a **Tags Input field**.
+# JavaScript Tags Input Library
+Native JavaScript library to make **Tags Input** Element in DOM.
+There isn't any dependency for this library, add it straight in your project and you'r ready to go.
 
+## Install via npm
+```bash
+    npm install vanilla-tags-input
+```
+## Import In ES6 or Greater
+```javascript
+    import TagsInput from 'vanilla-tags-input'
+```
+## Require in Node
+```javascript
+    let TagsInput = require('vanilla-tags-input')
+```
+## Import Styles
+```css
+    @import '~vanilla-tags-input/tags-input.css'
+```
+## Alternatively, you can use a CDN or even download
+```html
+<!-- Tags-input CSS -->
+<link rel="stylesheet" href="https://unpkg.com/vanilla-tags-input/tags-input.css">
+
+<!-- Tags-input JavaScript -->
+<script src="https://unpkg.com/vanilla-tags-input"></script>
+```
+
+# Usage
+`Selector` is the only compulsory option to making this plugin work, and it accepts **ID** of input field you want to convert to **tag input**.
+```javascript
+    var tagInput1 = new TagsInput({
+        selector: 'tag-input1',
+    });
+```
+However there are few other options too like
+* **Duplicate** - Set it to yes if you want to allow duplicate tags
+* **Max** - Limit the maximum number of tags to be added in plugin
+```javascript
+    var tagInput1 = new TagsInput({
+        selector: 'tag-input1',
+        duplicate : false,
+        max : 10
+    });
+```
+## Options
+Add multiple tags programatically
+```javascript
+    tagInput1.addData(['PHP' , 'JavaScript' , 'CSS'])
+    // or add single one
+    tagInput1.addTag('React');
+```
+
+Check if there are any erros to add a tag
+```javascript
+    tagInpu1.anyErrors('another tag');
+```
+Destroy the instance and release the cached memory
+```javascript
+    tagInput1.destroy();
+```
+Reinitialize the plugin if previously destroyed
+```javascript
+   tagInput1.init()
+
+    // you can reinitialize options
+    tagInput1.init({
+        selector: 'other-tag-input',
+        duplicate : false,
+        max : 10
+    })
+```
+Method chaning available too
+```javascript
+    tagsInput1.init().addTag().addData().destroy().init()
+```
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+Copyright (c) 2013-present, Yuxi (Evan) You
+
+
+## For Learning Purposes
 Here is a [Demo on codepen](https://codepen.io/iamqamarali/pen/qyawoR)
 
-Find [Tutorial](https://codingfacts.com/create-pure-javascript-plugin/) on how i build this awesome javascript plugin.
+Find [Tutorial](https://codingfacts.com/create-pure-javascript-plugin/) on how i build this javascript library.
--- tags-input.js
+++ tags-input.js
@@ -1,16 +1,46 @@
-(function(){
+(function( factory){
 
+    if(typeof module == 'object' && typeof module.exports == 'object' )
+        module.exports = factory();
+    else if( typeof window == 'object')
+        window.TagsInput = factory();
+    else
+        console.error('To use this library you need to either use browser or node.js [require()]');
+
+})(function(){
+    "use strict"
+
+    var initialized = false
+
+    // Plugin Constructor
     var TagsInput = function(opts){
         this.options = Object.assign(TagsInput.defaults , opts);
-        this.orignal_input = document.getElementById(opts.selector);
+        this.init();
+    }
+
+    // Initialize the plugin
+    TagsInput.prototype.init = function(opts){
+        this.options = opts ? Object.assign(this.options, opts) : this.options;
+
+        if(initialized)
+            this.destroy();
+            
+        if(!(this.orignal_input = document.getElementById(this.options.selector)) ){
+            console.error("tags-input couldn't find an element with the specified ID");
+            return this;
+        }
+
         this.arr = [];
         this.wrapper = document.createElement('div');
         this.input = document.createElement('input');
         init(this);
         initEvents(this);
+
+        initialized =  true;
+        return this;
     }
 
-
+    // Add Tags
     TagsInput.prototype.addTag = function(string){
 
         if(this.anyErrors(string))
@@ -19,13 +49,14 @@
         this.arr.push(string);
         var tagInput = this;
 
-
         var tag = document.createElement('span');
         tag.className = this.options.tagClass;
         tag.innerText = string;
 
         var closeIcon = document.createElement('a');
         closeIcon.innerHTML = '&times;';
+        
+        // delete the tag when icon is clicked
         closeIcon.addEventListener('click' , function(e){
             e.preventDefault();
             var tag = this.parentNode;
@@ -44,8 +75,7 @@
         return this;
     }
 
-
-
+    // Delete Tags
     TagsInput.prototype.deleteTag = function(tag , i){
         tag.remove();
         this.arr.splice( i , 1);
@@ -53,7 +83,7 @@
         return this;
     }
 
-
+    // Make sure input string have no error with the plugin
     TagsInput.prototype.anyErrors = function(string){
         if( this.options.max != null && this.arr.length >= this.options.max ){
             console.log('max tags limit reached');
@@ -68,7 +98,7 @@
         return false;
     }
 
-
+    // Add tags programmatically 
     TagsInput.prototype.addData = function(array){
         var plugin = this;
         
@@ -78,11 +108,29 @@
         return this;
     }
 
-    
+    // Get the Input String
     TagsInput.prototype.getInputString = function(){
         return this.arr.join(',');
     }
 
+
+    // destroy the plugin
+    TagsInput.prototype.destroy = function(){
+        this.orignal_input.removeAttribute('hidden');
+
+        delete this.orignal_input;
+        var self = this;
+        
+        Object.keys(this).forEach(function(key){
+            if(self[key] instanceof HTMLElement)
+                self[key].remove();
+            
+            if(key != 'options')
+                delete self[key];
+        });
+
+        initialized = false;
+    }
 
     // Private function to initialize the tag input plugin
     function init(tags){
@@ -92,8 +140,7 @@
         tags.orignal_input.parentNode.insertBefore(tags.wrapper , tags.orignal_input);
     }
 
-
-
+    // initialize the Events
     function initEvents(tags){
         tags.wrapper.addEventListener('click' ,function(){
             tags.input.focus();           
@@ -115,6 +162,7 @@
     }
 
 
+    // Set All the Default Values
     TagsInput.defaults = {
         selector : '',
         wrapperClass : 'tags-input-wrapper',
@@ -123,9 +171,5 @@
         duplicate: false
     }
 
-
-    window.TagsInput = TagsInput;
-
-})();
-
-   
+    return TagsInput;
+});
